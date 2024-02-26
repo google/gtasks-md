6c7c8b5c01363750c6a24bd54e7dd5d210051b81
jupblb <jupblb@google.com>
2024-02-26 17:24:03 +0100

    Simplify file structure

--- runner.py
+++ runner.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from app.cli import main
+from app import __main__
 
 if __name__ == "__main__":
-    main()
+    __main__.main()
