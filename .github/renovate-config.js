8f2a2a762acdd89a3d0f628c4ced9d25b12ee43a
Michal Kielbowicz <github@kielbowi.cz>
2026-08-31 08:40:36 +0200

    Switch Renovate to `GITHUB_TOKEN` for bot-authored PRs (#25)

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -2,4 +2,6 @@ module.exports = {
   platform: 'github',
   repositories: ['google/gtasks-md'],
   onboarding: false,
+  gitAuthor:
+    'github-actions <41898282+github-actions[bot]@users.noreply.github.com>',
 };
