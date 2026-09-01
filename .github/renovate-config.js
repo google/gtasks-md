c541b48a34be3aef2bc9a833c4e8d1a94a67983e
jupblb <git@kielbowi.cz>
2026-09-01 21:40:33 +0200

    Switch to Dependabot

--- .github/renovate-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  platform: 'github',
-  repositories: ['google/gtasks-md'],
-  onboarding: false,
-  gitAuthor:
-    'github-actions <41898282+github-actions[bot]@users.noreply.github.com>',
-};
